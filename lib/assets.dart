593ca3e221c197dd7e01a60ba7a796c87de1d10b
John Manning <john.d.manning@gmail.com>
2020-11-02 01:04:45 -0500

    feat: implement PDF reader using example defaults flutter_pdfview works, but flutter_cached_... won't load due to filesize

---
+++ lib/assets.dart
@@ -0,0 +1,4 @@
+class AppAssets {
+  static const String PROTOCOL_2019 =
+      'assets/pdf/EMS Protocols 2019 - WVEMS.pdf';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:wvems_protocols/ui/screens/pdf_state.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('WVEMS Protocols'),
-        ),
-      ),
+      home: PdfState(),
     );
   }
 }
