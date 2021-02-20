a2a65f115697f2a85cc93fe67d8313b6bdc19146
John Manning <john.d.manning@gmail.com>
2021-02-19 22:22:20 -0500

    feat: add freezed package + UI for union states

---
+++ lib/models/models.dart
@@ -0,0 +1,1 @@
+export 'pdf/pdf.dart';
--- lib/ui/views/pdf/pdf_state_controller.dart
+++ lib/ui/views/pdf/pdf_state_controller.dart
@@ -1,32 +1,22 @@
 import 'package:get/get.dart';
 import 'package:wvems_protocols/assets.dart';
+import 'package:wvems_protocols/models/models.dart';
 import 'package:wvems_protocols/services/services.dart';
 
 class PdfStateController extends GetxController {
   String pathPDF = '';
-  String landscapePathPdf = '';
-  String remotePDFpath = '';
-  String corruptedPathPDF = '';
 
   final PdfService _pdfService = PdfService();
 
+  final Rx<Pdf> pdfState = const Pdf.loading().obs;
+
   @override
   void onInit() {
-    // super.initState();
-    _pdfService.fromAsset(AppAssets.PROTOCOL_2020, 'corrupted.pdf').then((f) {
-      corruptedPathPDF = f.path;
-      update();
-    });
-    _pdfService.fromAsset(AppAssets.PROTOCOL_2020, 'demo.pdf').then((f) {
+    print('loading pdfs...');
+    _pdfService.fromAsset(AppAssets.PROTOCOL_2020, 'active.pdf').then((f) {
       pathPDF = f.path;
-      update();
-    });
-    _pdfService.fromAsset(AppAssets.PROTOCOL_2020, 'landscape.pdf').then((f) {
-      landscapePathPdf = f.path;
-      update();
-    });
-    _pdfService.createFileOfPdfUrl().then((f) {
-      remotePDFpath = f.path;
+      pdfState.value = Pdf.data(f);
+      print('pdf loaded: ${f.path}');
       update();
     });
     super.onInit();
