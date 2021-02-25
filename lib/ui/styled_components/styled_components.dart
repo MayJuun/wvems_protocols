4eee60ff5c0137b5faa1c7c78402a88d08d95480
Brian Ekey <brianekey@gmail.com>
2021-02-25 14:08:13 -0500

    fix: path name

--- lib/ui/styled_components/styled_components.dart
+++ lib/ui/styled_components/styled_components.dart
@@ -1,4 +1,4 @@
 export 'app_logo.dart';
 export 'styled_nav_drawer.dart';
 export 'styled_protocols_year.dart';
-export 'file:///C:/Users/Ekey/AndroidStudioProjects/wvems_protocols/lib/_internal/utils/wvems_colors.dart';
+export 'package:wvems_protocols/_internal/utils/wvems_colors.dart';
