914a1137c427d1a7fd9d89378512a0841f0cd962
John Manning <john.d.manning@gmail.com>
2021-03-22 08:38:40 -0400

    feat: connect searchController to pdfText as JSON

--- lib/controllers/search_controller.dart
+++ lib/controllers/search_controller.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:wvems_protocols/controllers/controllers.dart';
-import 'package:wvems_protocols/models/models.dart';
 
 class SearchController extends GetxController {
   final PdfStateController _pdfStateController = Get.find();
@@ -15,76 +14,69 @@ class SearchController extends GetxController {
 
   Future<List<Widget>> onQueryChanged(String newValue) async {
     // todo: reimplement
-    // if (newValue.length > 3) {
-    //   if (_pdfStateController.pdfDocState.value is PdfDocStateData) {
-    //     /// page number and the text from that page
-    //     final pageText = <int, String>{};
-    //     int pageNumber = 0;
-    //     for (var page
-    //         in (_pdfStateController.pdfDocState.value as PdfDocStateData)
-    //             .pdfDoc
-    //             .pages) {
-    //       pageText[pageNumber] = await page.text;
-    //       pageNumber++;
-    //     }
+    if (newValue.length > 3) {
+      _pdfStateController.pdfTextListState.value.when(data: (pageText) {
+        /// page number and a list of the strings matching the search string from
+        /// that particular page
+        final foundStrings = <Widget>[];
+        pageText.forEach(
+          (key, value) {
+            /// the indexes for this particular page where the search string is found
+            final List<int> indexes = [];
+            int curIndex = value.indexOf(newValue);
 
-    //     /// page number and a list of the strings matching the search string from
-    //     /// that particular page
-    //     final foundStrings = <Widget>[];
-    //     pageText.forEach(
-    //       (key, value) {
-    //         /// the indexes for this particular page where the search string is found
-    //         final List<int> indexes = [];
-    //         int curIndex = value.indexOf(newValue);
+            /// find the index of each matching string on a page
+            while (curIndex != -1) {
+              indexes.add(curIndex);
+              curIndex = value.indexOf(newValue, curIndex + 1);
+            }
 
-    //         /// find the index of each matching string on a page
-    //         while (curIndex != -1) {
-    //           indexes.add(curIndex);
-    //           curIndex = value.indexOf(newValue, curIndex + 1);
-    //         }
-
-    //         /// for each index on that page, create a substring. The substring
-    //         /// consists of 20 characters before the search string, 20 characters
-    //         /// after the search string, and then the search string itself is
-    //         /// displayed in bold
-    //         foundStrings.add(Text('PAGE $key'));
-    //         for (var i = 0; i < indexes.length; i++) {
-    //           foundStrings.add(
-    //             TextButton(
-    //               style: ButtonStyle(
-    //                   backgroundColor: MaterialStateProperty.all(
-    //                       i % 2 == 0 ? Colors.grey[300] : Colors.white)),
-    //               onPressed: () => null,
-    //               child: RichText(
-    //                 text: TextSpan(
-    //                   children: <TextSpan>[
-    //                     TextSpan(
-    //                         text:
-    //                             '...${pageText[key].substring(indexes[i] - 20 < 0 ? 0 : indexes[i] - 20, indexes[i])}'),
-    //                     TextSpan(
-    //                         text: pageText[key].substring(
-    //                             indexes[i],
-    //                             indexes[i] + newValue.length >=
-    //                                     pageText[key].length
-    //                                 ? pageText[key].length - 1
-    //                                 : indexes[i] + newValue.length),
-    //                         style:
-    //                             const TextStyle(fontWeight: FontWeight.bold)),
-    //                     TextSpan(
-    //                         text:
-    //                             '${pageText[key].substring(indexes[i] + newValue.length, indexes[i] + 20 + newValue.length >= pageText[key].length ? pageText[key].length - 1 : indexes[i] + 20 + newValue.length)}...'),
-    //                   ],
-    //                 ),
-    //               ),
-    //             ),
-    //           );
-    //         }
-    //       },
-    //     );
-    //     print('');
-    //     return foundStrings;
-    //   }
-    // }
+            /// for each index on that page, create a substring. The substring
+            /// consists of 20 characters before the search string, 20 characters
+            /// after the search string, and then the search string itself is
+            /// displayed in bold
+            // foundStrings.add(Text('PAGE $key'));
+            for (var i = 0; i < indexes.length; i++) {
+              foundStrings.add(
+                TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          i % 2 == 0 ? Colors.grey[300] : Colors.white)),
+                  onPressed: () => null,
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                            text:
+                                '...${pageText[key].substring(indexes[i] - 20 < 0 ? 0 : indexes[i] - 20, indexes[i])}'),
+                        TextSpan(
+                            text: pageText[key].substring(
+                                indexes[i],
+                                indexes[i] + newValue.length >=
+                                        pageText[key].length
+                                    ? pageText[key].length - 1
+                                    : indexes[i] + newValue.length),
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text:
+                                '${pageText[key].substring(indexes[i] + newValue.length, indexes[i] + 20 + newValue.length >= pageText[key].length ? pageText[key].length - 1 : indexes[i] + 20 + newValue.length)}...'),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+          },
+        );
+        print('');
+        return foundStrings;
+      }, loading: () {
+        //todo: handle loading state
+      }, error: (e, st) {
+        //todo: handle error state
+      });
+    }
 
     // todo: reimplement if (query == _query) { return; }
     // this will make it so that the query doesn't show isLoading content
@@ -98,7 +90,7 @@ class SearchController extends GetxController {
     // isLoading.value = false;
     // update();
 
-    // return [Container()];
+    return [Container()];
   }
 
   void clear() {
