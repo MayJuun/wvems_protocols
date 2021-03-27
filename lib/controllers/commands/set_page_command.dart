44ef11a1a428deaf559d7809d0396ae57d30fd34
Dokotela <jgfaulkenberry@gmail.com>
2021-03-26 22:27:19 -0400

    set_page_command null safe

--- lib/controllers/commands/set_page_command.dart
+++ lib/controllers/commands/set_page_command.dart
@@ -2,7 +2,7 @@ import 'package:wvems_protocols/controllers/commands/abstract_command.dart';
 
 class SetPageCommand extends AbstractCommand {
   @override
-  Future<void> execute({int pageNum}) async {
+  Future<void> execute({int? pageNum}) async {
     await pdfStateController.rxPdfController.value.setPage(pageNum ?? 0);
     pdfStateController.update();
   }
