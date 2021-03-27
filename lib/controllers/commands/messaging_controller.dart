6a6f82489241ce847a5a7c430f3e98c13fd0fc3f
Dokotela <jgfaulkenberry@gmail.com>
2021-03-27 00:50:02 -0400

    Received firebase messaging

---
+++ lib/controllers/commands/messaging_controller.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+
+class MessagingController extends GetxController {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  late NotificationSettings settings;
+  final AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    'This channel is used for important notifications.', // description
+    importance: Importance.max,
+  );
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  Future<void> onInit() async {
+    settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+    super.onInit();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      final RemoteNotification? notification = message.notification;
+      final AndroidNotification? android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (notification != null && android != null) {
+        Get.snackbar(notification.title ?? '', notification.body ?? '');
+        print('${notification.title ?? ''} ${notification.body ?? ''}');
+
+        //   flutterLocalNotificationsPlugin.show(
+        //       notification.hashCode,
+        //       notification.title,
+        //       notification.body,
+        //       NotificationDetails(
+        //         android: AndroidNotificationDetails(
+        //           channel.id,
+        //           channel.name,
+        //           channel.description,
+        //           icon: android.smallIcon,
+        //         ),
+        //       ));
+      }
+      // });
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:wvems_protocols/controllers/commands/messaging_controller.dart';
 import 'package:wvems_protocols/controllers/controllers.dart';
 import 'package:wvems_protocols/ui/views/views.dart';
 
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
 
         // Once complete, show your application
         else if (snapshot.connectionState == ConnectionState.done) {
+          Get.put<MessagingController>(MessagingController());
           return GetMaterialApp(
             theme: ThemeController.to.lightTheme.themeData,
             darkTheme: ThemeController.to.darkTheme.themeData,
