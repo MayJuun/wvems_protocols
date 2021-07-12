bf773322359d949c56eb1091ca0f7e1b1763c9fb
ZapsApps <ZapsDevAcct@gmail.com>
2021-07-12 14:16:32 -0400

    Update cloud_storage_service.dart
    Process to check for local pdf files

--- lib/services/firebase/cloud_storage_service.dart
+++ lib/services/firebase/cloud_storage_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path_provider/path_provider.dart';
 
 // Use the Firebase controller to access methods within this service.
 class CloudStorageService {
@@ -9,12 +10,35 @@ class CloudStorageService {
     //Simple Print Statement - Zaps
     print('Ready to list examples!');
 
+    // List of file names
+    List pdfFileList = [];
+
+    // Get the app directory
+    final directory = await getApplicationDocumentsDirectory();
+
+    // Get pdf filenames
     result.items.forEach((Reference ref) {
-      print('Found file: $ref');
+      String tmpFileName = ref.fullPath;
+      const start = 'pdf/';
+      tmpFileName = tmpFileName.substring(start.length);
+      tmpFileName = directory.path + '/' + tmpFileName;
+      pdfFileList.add(tmpFileName);
     });
 
     result.prefixes.forEach((Reference ref) {
       print('Found directory: $ref');
     });
+
+    // Get list of items in app directory
+    List contents = directory.listSync();
+
+    // Check to see if filenames match contents of app directory
+    for (var fileName in pdfFileList) {
+      if (contents.contains(fileName)) {
+        print('File: ' + fileName + ' is here!');
+      } else {
+        print('File: ' + fileName + ' is not here!');
+      }
+    }
   }
 }
