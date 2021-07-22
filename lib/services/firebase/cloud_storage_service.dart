76c731ae353f66b4792f7acb6e10411d662ea2d5
ZapsApps <49655139+ZapsApps@users.noreply.github.com>
2021-07-22 12:08:06 -0400

    Update cloud storage service
    This enables the downloading of JSON files

--- lib/services/firebase/cloud_storage_service.dart
+++ lib/services/firebase/cloud_storage_service.dart
@@ -8,7 +8,12 @@ class CloudStorageService {
 
   Future<void> listExample() async {
     // List of items in the storage reference
+
+    // PDF Files in Firebase Storage
     final ListResult result = await storage.ref().child('pdf').listAll();
+
+    // JSON Files in Firebase Storage
+    final ListResult jsonResult = await storage.ref().child('json').listAll();
 
     //Simple Print Statement - Zaps
     print('Ready to list examples!');
@@ -25,21 +30,38 @@ class CloudStorageService {
       String tmpFileName = ref.fullPath;
       print(tmpFileName + ' is available in Firebase Storage');
 
-      // Download files anyway (force for testing)
-
       // Get filename and alter to match the Application Doc directory
       const start = 'pdf/';
       tmpFileName = tmpFileName.substring(start.length);
       tmpFileName = directory.path + '/' + tmpFileName;
-      //print(tmpFileName);
 
       if (contents.contains(tmpFileName)) {
-        print('Here!');
+        print(tmpFileName + ' is here!');
       } else {
-          print('Not Here!');
-          print('Downloading');
-          File downloadToFile = File(tmpFileName);
-          ref.writeToFile(downloadToFile);
+        print(tmpFileName + ' is not here!');
+        print('Downloading');
+        File downloadToFile = File(tmpFileName);
+        ref.writeToFile(downloadToFile);
+      }
+    });
+
+    jsonResult.items.forEach((Reference ref) {
+      String tmpJSONFileName = ref.fullPath;
+      print(tmpJSONFileName + ' is available in Firebase Storage');
+
+      // Get filename and alter to match the Application Doc directory
+      const start = 'json/';
+      tmpJSONFileName = tmpJSONFileName.substring(start.length);
+      tmpJSONFileName = directory.path + '/' + tmpJSONFileName;
+      print(tmpJSONFileName);
+
+      if (contents.contains(tmpJSONFileName)) {
+        print(tmpJSONFileName + ' is here!');
+      } else {
+        print(tmpJSONFileName + ' is not here!');
+        print('Downloading');
+        File downloadToFile = File(tmpJSONFileName);
+        ref.writeToFile(downloadToFile);
       }
     });
 
